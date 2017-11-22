a57c98a83d8b34ecb8e306acd8020ff2421932e2
anish <anish.vahora5@gmail.com>
2017-11-22 19:36:03 +0530

    init

--- app/src/main/java/com/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/kotlindemo/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.kotlindemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
 
+/**
+ * val is used as final when you know its value is not gonna change
+ * var is used as a variable when you need to change its value
+ */
 class MainActivity : AppCompatActivity() {
+
+    var pCount = 0 //println count
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,17 +25,44 @@ class MainActivity : AppCompatActivity() {
 
         val btn = findViewById<Button>(R.id.btnId) as Button
         btn.setOnClickListener(View.OnClickListener { view ->
-            println("name=" + name)// prints your name
-            println("age =$age")// prints your age using $ string concatenation type
+            println("(${pCount++}) name=" + name)// prints your name. And ${pCount++} depicts under string calculation
+            println("(${pCount++}) age =$age")// prints your age using $ string concatenation type
             getIntInfo()
         })
     }
 
     private fun getIntInfo() {
-        var bigInt: Int = Int.MAX_VALUE
-        var smallInt: Int = Int.MIN_VALUE
+        val bigInt: Int = Int.MAX_VALUE
+        val smallInt: Int = Int.MIN_VALUE
 
-        println("bigInt :: $bigInt smallInt :: $smallInt")
+        println("(${pCount++}) bigInt :: $bigInt smallInt :: $smallInt")
+
+        val letterGrade = 'A'
+        println("(${pCount++}) A is a char : $letterGrade")
+        println("(${pCount++}) A is a char : ${letterGrade is Char}") //actually its always true .. just to show "instance of" functionality
+        println("(${pCount++}) 65 to Char: " + 65.toChar())
+        println("(${pCount++}) A to Int: " + 'A'.toInt())
+        println("(${pCount++}) 3.14 to Int: " + 3.14.toInt())
+
+        showUnderStringCalculation()
+    }
+
+    private fun showUnderStringCalculation() {
+        println("(${pCount++}) 1 + 2 is ${1 + 2}") //under string calculation
+
+        var a = "A"
+        var b = "B"
+        var c = "Blah Blah"
+        var d = "A"
+        println("(${pCount++}) is a length = b length ${(a.length) == (b.length)}")
+        println("(${pCount++}) is a length = c length ${(a.length) == (c.length)}")
+
+        println("(${pCount++}) is a equals b ${a.equals(b)}")
+        println("(${pCount++}) is a == b ${a == b}")//will result same as above, no issue with == and equals
+
+        println("(${pCount++})  value A compareTo value B is ${a.compareTo(b)}")
+        println("(${pCount++}) value B compareTo value A is ${b.compareTo(a)}")
+        println("(${pCount++})  value A compareTo A is ${a.compareTo(d)}")
 
     }
 }
