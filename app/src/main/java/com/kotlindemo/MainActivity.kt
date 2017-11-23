79702af1e2e0496e0d4757d9d9d8291b42da70d5
anish <anish.vahora5@gmail.com>
2017-11-23 19:29:02 +0530

    init

--- app/src/main/java/com/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/kotlindemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
+import java.lang.reflect.Array
 
 /**
  * val is used as final when you know its value is not gonna change
@@ -25,10 +26,78 @@ class MainActivity : AppCompatActivity() {
 
         val btn = findViewById<Button>(R.id.btnId) as Button
         btn.setOnClickListener(View.OnClickListener { view ->
-            println("(${pCount++}) name=" + name)// prints your name. And ${pCount++} depicts under string calculation
-            println("(${pCount++}) age =$age")// prints your age using $ string concatenation type
+            valVarInfo(name, age)
             getIntInfo()
+            showUnderStringCalculation()
+            stringManipulation()
+            showArrayDemo()
+            arrayRangeDemo()
+
         })
+    }
+
+    private fun arrayRangeDemo() {
+        val oneTo10 = 1..10
+        val mOneTo10 = 1.rangeTo(10) //same thing
+        val alpha = "A".rangeTo("Z") //or "A".."Z"
+
+        println("(${pCount++})_________________________")
+        for (i in oneTo10) {
+            println("oneTo10 : $i")
+        }
+        println("_____________________________")
+
+
+        for (i in mOneTo10) {
+            println("mOneTo10 : $i")
+        }
+
+        println("_____________________________")
+
+        //increment range with step difference
+
+        val stepValues = 1.rangeTo(10).step(3)
+        for (i in stepValues) {
+            println("stepValues : $i")
+        }
+        println("_____________________________")
+
+        for(i in stepValues.reversed()){
+            println("reversed step values: $i")
+        }
+    }
+
+    private fun showArrayDemo() {
+        var myArray = arrayOf(1, 2.78, "Anish Vahora", true)
+
+        println("(${pCount++}) myArray size = ${myArray.size}")
+        println("(${pCount++}) array element at 2 position = ${myArray[2]}")
+        println("(${pCount++}) if array contains 2.78 ${myArray.contains(2.78)}")
+
+        val partArray = myArray.copyOfRange(0, 3)
+        println("------------------------------")
+        for (any in partArray) {
+            println("(${pCount++}) elements of partArray = $any")
+        }
+        println("---*************************---")
+
+        val squareArray = Array(5, { x -> x * x }) //look for the syntax Array() instead arrayOf
+        println("(${pCount++})  index square in array (size 5) kept is  = ${squareArray[3]}")
+
+
+    }
+
+    private fun stringManipulation() {
+        val rs = "Random String"
+        println("(${pCount++}) At 2nd index of 'Random String'=  + ${rs.get(1)}")
+        println("(${pCount++}) At 2nd index of 'Random String'=  + ${rs[1]}") //another better way to represent the same
+        println("(${pCount++}) show string from index 3 to 7=  + ${rs.subSequence(3, 8)}")
+        println("(${pCount++}) doest it contains 'dom' : ${rs.contains("dom")}")
+    }
+
+    private fun valVarInfo(name: String, age: Int) {
+        println("(${pCount++}) name=" + name)// prints your name. And ${pCount++} depicts under string calculation
+        println("(${pCount++}) age =$age")// prints your age using $ string concatenation type
     }
 
     private fun getIntInfo() {
@@ -44,16 +113,15 @@ class MainActivity : AppCompatActivity() {
         println("(${pCount++}) A to Int: " + 'A'.toInt())
         println("(${pCount++}) 3.14 to Int: " + 3.14.toInt())
 
-        showUnderStringCalculation()
     }
 
     private fun showUnderStringCalculation() {
         println("(${pCount++}) 1 + 2 is ${1 + 2}") //under string calculation
 
-        var a = "A"
-        var b = "B"
-        var c = "Blah Blah"
-        var d = "A"
+        val a = "A"
+        val b = "B"
+        val c = "Blah Blah"
+        val d = "A"
         println("(${pCount++}) is a length = b length ${(a.length) == (b.length)}")
         println("(${pCount++}) is a length = c length ${(a.length) == (c.length)}")
 
